74edff674bdb14c48d00e5fdba498c5bdfbbc23e
Mingye Chen <mingyechen82@gmail.com>
2023-11-28 17:32:37 -0500

    Add example

--- hw2/benchmarks/correctness/hw2correct1.c
+++ hw2/benchmarks/correctness/hw2correct1.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
-int main(){
-	int A[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-	int B[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-	int i, j;
-	j = 0;
-	for(i = 0; i < 10; i++) {
-  		B[i] = A[j] * 11 + i;
-  		if(i < 8) 
-  			j = i;
-		printf("%d\n", B[i]);
+int main()
+{
+	int x = 2;
+	int y = 3;
+	int z = 4;
+	if (x == 2)
+	{
+		z = y;
+	}
+	else
+	{
+		z = 5;
 	}
 	return 0;
 }
