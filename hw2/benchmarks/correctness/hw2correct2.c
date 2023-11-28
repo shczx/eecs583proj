84a9a5889f78c6d1ae9b810c43b70ce4c6b8b83b
Mingye Chen <mingyechen82@gmail.com>
2023-11-28 18:50:02 -0500

    Update tests
    Co-authored-by: Yichen <Tiger32828@users.noreply.github.com>
    Co-authored-by: shczx <shczx@users.noreply.github.com>
    Co-authored-by: zfh005 <zfh005@users.noreply.github.com>

--- hw2/benchmarks/correctness/hw2correct2.c
+++ hw2/benchmarks/correctness/hw2correct2.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
-int main(){
-	int A[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-	int B[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-	int i, j;
-	j = 0;
-	for(i = 0; i < 10; i++) {
-  		B[i] = A[j] * 13 + 4 + i;
-  		if(i % 8 == 0) 
-  			j = i;
-		printf("%d\n", B[i]);
+int main()
+{
+	int x = 2;
+	int y = 3;
+	int z = 4;
+	if (x == 2)
+	{
+		z = y;
 	}
+	else
+	{
+		y = 6;
+	}
+	z = y;
 	return 0;
 }
